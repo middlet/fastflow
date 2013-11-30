cc17515486df8c2fca4e0be71b6807a25a29b993
lee <ljm@it-innovation.soton.ac.uk>
2013-11-30 09:20:46 +0000

    first commit of the files. just a test rig for the code

---
+++ src/fastflow.cpp
@@ -0,0 +1,10 @@
+/*!
+    \file fastflow.cpp
+
+    this is the main code to generate the flow information from an
+    image sequence
+
+*/
+
+// internal includes
+# include "fastflow.h"
---
+++ src/fastflow.h
@@ -0,0 +1,12 @@
+/*!
+    \file fastflow.h
+
+    this is the main code to generate the flow information from an
+    image sequence
+*/
+
+# ifndef FFLOW_FASTFLOW_H
+# define FFLOW_FASTFLOW_H
+
+
+# endif // FFLOW_FASTFLOW_H
