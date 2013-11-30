3c15319ad68583c874a268ece9f7de1adadc996d
lee <ljm@it-innovation.soton.ac.uk>
2013-11-30 09:29:06 +0000

    added a main method to call the fast flow algorithm. does minimal option checking

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+/*!
+    \file main.cpp
+
+    main program to run the code
+
+*/
+
+# include "fastflow.h"
+
+int
+main (int argc, char *argv[])
+{
+    if (argc<2)
+        std::cerr << "not enough arguments\n" << "usage : ./fflow <file>"
+ << std::endl;
+}
