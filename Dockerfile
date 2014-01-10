8cdef92f15f26e5a1d8c52687c7c45ec15e483bc
middlet <middlet@gmail.com>
2014-01-10 09:37:44 +0000

    now have a working dockerfile for building a container

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+# opencv
+#
+# building:
+#	docker build -t <name> .
+#
+# running with similar mounting to vagrant
+#	docker run -v `pwd`:/vagrant -t name -i /bin/bash
+
+from		ubuntu:12.04
+maintainer	middlet@gmail.com
+
+run			echo "deb http://archive.ubuntu.com/ubuntu precise universe multiverse" >> /etc/apt/sources.list
+
+run			apt-get update
+run			apt-get install -y wget
+
+add			./build_opencv.bash /tmp/build_opencv.bash
+run			/bin/bash /tmp/build_opencv.bash
--- build_opencv.bash
+++ build_opencv.bash
@@ -21,12 +21,12 @@ mkdir opencv
 cd opencv
 
 echo "installing dependencies"
-sudo apt-get -q update
-sudo apt-get -q upgrade
-sudo apt-get -y install libopencv-dev build-essential checkinstall cmake pkg-config yasm libtiff4-dev libjpeg-dev libjasper-dev libavcodec-dev libavformat-dev libswscale-dev libdc1394-22-dev libxine-dev libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev libv4l-dev python-dev python-numpy libtbb-dev libqt4-dev libgtk2.0-dev libfaac-dev libmp3lame-dev libopencore-amrnb-dev libopencore-amrwb-dev libtheora-dev libvorbis-dev libxvidcore-dev x264 v4l-utils ffmpeg unzip
+apt-get -q update
+apt-get -q upgrade
+apt-get -y install build-essential checkinstall cmake pkg-config yasm libtiff4-dev libjpeg-dev libjasper-dev libavcodec-dev libavformat-dev libswscale-dev libdc1394-22-dev libxine-dev libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev libv4l-dev python-dev python-numpy libtbb-dev libqt4-dev libgtk2.0-dev libfaac-dev libmp3lame-dev libopencore-amrnb-dev libopencore-amrwb-dev libtheora-dev libvorbis-dev libxvidcore-dev x264 v4l-utils ffmpeg unzip
 
 echo "downloading opencv" $version
-wget -O $downloadfile http://sourceforge.net/projects/opencvlibrary/files/opencv-unix/$version/$downloadfile/download
+wget --quiet -O $downloadfile http://sourceforge.net/projects/opencvlibrary/files/opencv-unix/$version/$downloadfile/download
 
 echo "installing opencv" $version
 echo $downloadfile | grep ".zip"
@@ -41,10 +41,10 @@ cd build
 
 cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D WITH_TBB=ON -D BUILD_NEW_PYTHON_SUPPORT=ON -D WITH_V4L=ON -D INSTALL_C_EXAMPLES=ON -D INSTALL_PYTHON_EXAMPLES=ON -D BUILD_EXAMPLES=ON -D WITH_QT=ON -D WITH_OPENGL=ON ..
 make
-sudo make install
-sudo sh -c 'echo "/usr/local/lib" > /etc/ld.so.conf.d/opencv.conf'
-sudo ldconfig
+make install
+sh -c 'echo "/usr/local/lib" > /etc/ld.so.conf.d/opencv.conf'
+ldconfig
 echo "opencv" $version "ready to be used"
 
 echo "installing code dependencies"
-sudo apt-get -y install libboost-filesystem-dev libboost-system-dev
+apt-get -y install libboost-filesystem-dev libboost-system-dev
